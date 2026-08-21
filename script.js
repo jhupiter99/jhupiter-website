499c7d3aaa9e7a6b4fd2c338505efb5abb9667f0
jhupiter99 <jhupiter99@gmail.com>
2026-08-21 06:29:10 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,55 @@
 window.addEventListener("load", function () {
     const loader = document.getElementById("loader");
 
-    setTimeout(function () {
-        loader.style.display = "none";
-    }, 500);
+    if (loader) {
+        setTimeout(function () {
+            loader.style.display = "none";
+        }, 500);
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("#nav-menu a");
+
+window.addEventListener("scroll", function () {
+
+    let current = "";
+
+    sections.forEach(function (section) {
+        const sectionTop = section.offsetTop - 150;
+
+        if (window.scrollY >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(function (link) {
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+    });
+
+});
+
+    const menuToggle = document.getElementById("menu-toggle");
+    const navMenu = document.getElementById("nav-menu");
+
+    if (menuToggle && navMenu) {
+
+        menuToggle.addEventListener("click", function () {
+            navMenu.classList.toggle("active");
+        });
+
+        const navLinks = navMenu.querySelectorAll("a");
+
+        navLinks.forEach(function (link) {
+            link.addEventListener("click", function () {
+                navMenu.classList.remove("active");
+            });
+        });
+    }
+
 });
