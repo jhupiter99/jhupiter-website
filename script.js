a0401f26484021e1ddaef12520c633aadc5d152a
jhupiter99 <jhupiter99@gmail.com>
2026-08-21 00:59:02 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function () {
+    const loader = document.getElementById("loader");
+
+    setTimeout(function () {
+        loader.style.display = "none";
+    }, 500);
+});
